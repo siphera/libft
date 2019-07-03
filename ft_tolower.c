d303df738dbef9641cb78b574a002bcedb7d39c7
siphenkosi salman <ssalman@c3r6s1.wethinkcode.co.za>
2019-07-04 01:42:57 +0200

    added  ft_isascii.c file

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,13 @@
 /*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:58:38 by ssalman           #+#    #+#             */
-/*   Updated: 2019/07/04 01:29:29 by ssalman          ###   ########.fr       */
+/*   Updated: 2019/07/04 01:41:47 by ssalman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_tolower(int c)
+int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
