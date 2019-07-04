6314bcdd28c1b460990986bc3f9d8d5d842f2498
siphenkosi salman <ssalman@c3r6s1.wethinkcode.co.za>
2019-07-04 06:29:31 +0200

    modified memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,8 +6,13 @@
 /*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 15:30:34 by ssalman           #+#    #+#             */
-/*   Updated: 2019/07/04 01:29:07 by ssalman          ###   ########.fr       */
+/*   Updated: 2019/07/04 06:28:47 by ssalman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void    *ft_memset(void *b, int c, size_t len)
+{
+    
+}
