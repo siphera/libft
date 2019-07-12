10e6be30b6542da50285d8ef4d57c032787ec74f
siphenkosi salman <ssalman@c3r8s1.wethinkcode.co.za>
2019-07-12 05:17:11 +0200

    added few functyions

---
+++ ft_striteri.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 05:15:58 by ssalman           #+#    #+#             */
+/*   Updated: 2019/07/12 05:16:26 by ssalman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int index;
+
+	index = 0;
+	if (s != NULL && f != NULL)
+	{
+		while (s[index] != 0)
+		{
+			f(index, &s[index]);
+			index++;
+		}
+	}
+}
