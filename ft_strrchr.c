55d27fcf4fc864ac33b20a8b51fa7a193ce20d07
siphenkosi salman <ssalman@c3r8s1.wethinkcode.co.za>
2019-07-12 04:51:45 +0200

    strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 04:50:38 by ssalman           #+#    #+#             */
+/*   Updated: 2019/07/12 04:51:00 by ssalman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int pos;
+
+	pos = ft_strlen(s);
+	if (s)
+	{
+		while (s[pos] != (char)c && pos >= 0)
+			pos--;
+		if (s[pos] == (char)c)
+			return ((char*)&s[pos]);
+		else
+			return (NULL);
+	}
+	return (NULL);
+}
