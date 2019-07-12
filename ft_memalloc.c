f15c5c265a227a7683fad692a2002f8f09da347f
siphenkosi salman <ssalman@c3r8s1.wethinkcode.co.za>
2019-07-12 05:10:47 +0200

    memmalloc and memdel

---
+++ ft_memalloc.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 05:07:44 by ssalman           #+#    #+#             */
+/*   Updated: 2019/07/12 05:08:12 by ssalman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void *dest;
+	char *clean;
+
+	dest = malloc(size);
+	clean = dest;
+	if (dest)
+	{
+		while (size != 0)
+		{
+			*clean++ = 0;
+			size--;
+		}
+		return (dest);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
