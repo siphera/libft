56171cce2809a35fa41bb784231685142464c551
siphenkosi salman <ssalman@c3r8s1.wethinkcode.co.za>
2019-07-12 05:00:48 +0200

    strcmp

---
+++ ft_strcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 04:59:08 by ssalman           #+#    #+#             */
+/*   Updated: 2019/07/12 05:00:13 by ssalman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while ((unsigned char)*s1 == (unsigned char)*s2 && *s1 != 0)
+	{
+		s1++;
+		s2++;
+	}
+	if ((unsigned char)*s1 == (unsigned char)*s2)
+		return (0);
+	else
+		return ((unsigned char)*s1 - (unsigned char)*s2);
+}
