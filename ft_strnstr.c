19af8e0be6d29bf01b47b3508a10e24f80ef68ca
siphenkosi salman <ssalman@c3r8s1.wethinkcode.co.za>
2019-07-12 04:56:57 +0200

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 04:55:45 by ssalman           #+#    #+#             */
+/*   Updated: 2019/07/12 04:56:22 by ssalman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	if (*needle == 0)
+		return ((char*)haystack);
+	while (haystack[i] != 0 && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && needle[j] != 0 && j + i < len)
+			j++;
+		if (j == ft_strlen(needle))
+			return ((char*)&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
