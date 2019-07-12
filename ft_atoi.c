afc84579558c904f524b5bc359478a1e6708697a
siphenkosi salman <ssalman@c3r8s1.wethinkcode.co.za>
2019-07-12 04:02:14 +0200

    modified atoi + memset and deleted isspace

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,15 +6,39 @@
 /*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 02:10:59 by ssalman           #+#    #+#             */
-/*   Updated: 2019/07/11 03:50:19 by ssalman          ###   ########.fr       */
+/*   Updated: 2019/07/12 03:56:51 by ssalman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+static int	my_isspace(char c)
 {
-    int i;
-    int num;
-    int sign;
+	if (c == '\t' || c == '\n' || c == '\r')
+		return (1);
+	else if (c == '\f' || c == '\v' || c == ' ')
+		return (1);
+	else
+		return (0);
+}
+
+int			ft_atoi(const char *str)
+{
+	int res;
+	int negative;
+
+	negative = 1;
+	res = 0;
+	while (my_isspace(*str))
+		str++;
+	if (*str == '-')
+	{
+		negative = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+		res = (res * 10) + (*str++ - 48);
+	return (negative * res);
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,21 @@
 /*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 15:30:34 by ssalman           #+#    #+#             */
-/*   Updated: 2019/07/04 06:28:47 by ssalman          ###   ########.fr       */
+/*   Updated: 2019/07/12 04:00:43 by ssalman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memset(void *b, int c, size_t len)
+void *ft_memset(void *b, int c, size_t len)
 {
-    
+	char *a;
+
+	a = (char *)b;
+	while (len > 0)
+	{
+		*a++ = (char)c;
+		len--;
+	}
+	return (b);
 }
