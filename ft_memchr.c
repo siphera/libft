023ea20770964d66c2fdc5830fd4a2bd21764396
siphenkosi salman <ssalman@c3r8s1.wethinkcode.co.za>
2019-07-12 04:18:35 +0200

    memchr

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 04:16:34 by ssalman           #+#    #+#             */
+/*   Updated: 2019/07/12 04:16:49 by ssalman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *str;
+
+	while (n-- > 0)
+	{
+		str = (unsigned char*)s;
+		if (*str == (unsigned char)c)
+		{
+			return (str);
+		}
+		s++;
+	}
+	return (NULL);
+}
